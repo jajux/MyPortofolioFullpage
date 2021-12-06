9b21ad78c23aa2742fc5935830b715915bfa5bed
Jawad EL HATTAB <jawad.elhattab@hotmail.fr>
2021-12-06 08:23:06 +0100

    setup responsive hero page and nav

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Hamburger open/close
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__link');
+
